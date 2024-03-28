485deb9ac94b557c1e6cb239c7407a189cb6b11f
caleb-kuss <kusscall@gmail.com>
2024-03-27 21:32:06 -0500

    feat: add rover details button for mars photo exploration

--- src/types/MarsRover/marsRover.d.ts
+++ src/types/MarsRover/marsRover.d.ts
@@ -73,3 +73,35 @@ export type FavoriteMarsPhoto = {
   marsRoverData: MarsPhoto;
   marsRoverDataId: String;
 };
+
+export type MarsRover = {
+  id: number;
+  sol: number;
+  img_src: string;
+  earth_date: string;
+  camera: {
+    id: number;
+    name: string;
+    rover_id: number;
+    full_name: string;
+  };
+  rover: {
+    id: number;
+    name: string;
+    landing_date: string;
+    launch_date: string;
+    status: string;
+    max_sol: number;
+    max_date: string;
+    total_photos: number;
+    cameras: [
+      { name: string; full_name: string },
+      { name: string; full_name: string },
+      { name: string; full_name: string },
+      { name: string; full_name: string },
+      { name: string; full_name: string },
+      { name: string; full_name: string },
+      { name: string; full_name: string }
+    ];
+  };
+};
