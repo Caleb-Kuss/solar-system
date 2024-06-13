798c03eacd71b7b10ae960239ec1b3de1652030b
caleb-kuss <kusscall@gmail.com>
2024-06-12 21:47:15 -0500

    chore: add stats about apod and rover likes

--- src/app/actions/admin.ts
+++ src/app/actions/admin.ts
@@ -4,6 +4,7 @@ import { PrismaClient } from "@prisma/client";
 import { getServerSession } from "next-auth/next";
 import { options } from "@/app/api/auth/[...nextauth]/options";
 import { Session } from "@/types/Users/users";
+import moment from "moment";
 const prisma = new PrismaClient();
 
 export async function totalFavoriteApods() {
@@ -39,4 +40,110 @@ export async function totalFavoriteRoverImages() {
     console.error("Error getting total favorite rover images:", error);
     throw error;
   }
+}
+
+// This only counts new records
+export async function DailyApodLikes() {
+  const session: Session = await getServerSession(options);
+
+  if (!session || session?.user?.role !== "ADMIN") {
+    throw new Error(
+      "Unauthorized access: User does not have admin privileges.",
+    );
+  }
+  const today = moment().startOf("day").toDate();
+  const tomorrow = moment(today).add(1, "day").toDate();
+
+  const favoriteApodsCount = await prisma.favoriteApod.count({
+    where: {
+      apod: {
+        createdAt: {
+          gte: today,
+          lt: tomorrow,
+        },
+      },
+    },
+  });
+
+  return favoriteApodsCount;
+}
+
+export async function WeeklyApodLikes() {
+  const session: Session = await getServerSession(options);
+
+  if (!session || session?.user?.role !== "ADMIN") {
+    throw new Error(
+      "Unauthorized access: User does not have admin privileges.",
+    );
+  }
+  const startOfThisWeek = moment().startOf("isoWeek");
+  const startOfLastWeek = startOfThisWeek.clone().subtract(1, "week");
+
+  const endOfLastWeek = startOfThisWeek.clone().subtract(1, "day").endOf("day");
+
+  const favoriteApodsCount = await prisma.favoriteApod.count({
+    where: {
+      apod: {
+        createdAt: {
+          gte: startOfLastWeek.toDate(),
+          lt: endOfLastWeek.toDate(),
+        },
+      },
+    },
+  });
+
+  return favoriteApodsCount;
+}
+
+// This only counts new records
+export async function DailyRoverLikes() {
+  const session: Session = await getServerSession(options);
+
+  if (!session || session?.user?.role !== "ADMIN") {
+    throw new Error(
+      "Unauthorized access: User does not have admin privileges.",
+    );
+  }
+  const today = moment().startOf("day").toDate();
+  const tomorrow = moment(today).add(1, "day").toDate();
+
+  const favoriteRoverImagesCount = await prisma.favoriteMarsRoverData.count({
+    where: {
+      marsRoverData: {
+        createdAt: {
+          gte: today,
+          lt: tomorrow,
+        },
+      },
+    },
+  });
+
+  return favoriteRoverImagesCount;
+}
+
+export async function WeeklyRoverLikes() {
+  const session: Session = await getServerSession(options);
+
+  if (!session || session?.user?.role !== "ADMIN") {
+    throw new Error(
+      "Unauthorized access: User does not have admin privileges.",
+    );
+  }
+  const startOfThisWeek = moment().startOf("isoWeek");
+  const startOfLastWeek = startOfThisWeek.clone().subtract(1, "week");
+
+  const endOfLastWeek = startOfThisWeek.clone().subtract(1, "day").endOf("day");
+
+  const favoriteRoverImagesCount = await prisma.favoriteMarsRoverData.count({
+    where: {
+      marsRoverData: {
+        createdAt: {
+          gte: startOfLastWeek.toDate(),
+          lt: endOfLastWeek.toDate(),
+        },
+      },
+    },
+  });
+
+  return favoriteRoverImagesCount;
 }
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,5 @@
 
-import { totalFavoriteApods, totalFavoriteRoverImages } from "../actions/admin";
+import { totalFavoriteApods, totalFavoriteRoverImages, DailyApodLikes, WeeklyApodLikes, DailyRoverLikes, WeeklyRoverLikes } from "../actions/admin";
 import { getServerSession } from "next-auth/next"
 import { options } from "../api/auth/[...nextauth]/options";
 import { Session } from "@/types/Users/users";
@@ -22,7 +22,10 @@ export default async function AdminPage() {
 
   const totalApods = await totalFavoriteApods()
   const totalRoverImages = await totalFavoriteRoverImages()
-
+  const apodLikes = await DailyApodLikes()
+  const apodLikesLastWeek = await WeeklyApodLikes()
+  const roverLikes = await DailyRoverLikes()
+  const roverLikesLastWeek = await WeeklyRoverLikes()
   return (
     <>
       <Navbar />
@@ -30,14 +33,32 @@ export default async function AdminPage() {
         <h4 className="flex justify-center mb-10 text-base md:text-3xl">
           Admin Dashboard for {session?.user?.name}
         </h4>
+        <h1 className="flex justify-center text-xl font-bold mb-4">APOD Stats</h1>
         <div className="flex flex-col  md:items-center justify-center mb-4">
           <div className="flex flex-col items-center justify-center mb-4">
-            <h3 className="text-xl font-bold mb-2">Total Favorite Apods</h3>
+            <h3 className="text-l font-bold mb-2">Total Favorite Apods</h3>
             <p className="text-2xl">{totalApods}</p>
           </div>
-          <div className="flex flex-col items-center justify-center">
-            <h3 className="text-xl font-bold mb-2">Total Favorite Rover Images</h3>
+          <div className="flex flex-col items-center justify-center mb-4">
+            <h3 className="text-l font-bold mb-2">Total Favorite Apod Likes Today</h3>
+            <p className="text-2xl">{apodLikes}</p>
+          </div>
+          <div className="flex flex-col items-center justify-center mb-4">
+            <h3 className="text-l font-bold mb-2">Total Favorite Apod Likes Last Week</h3>
+            <p className="text-2xl">{apodLikesLastWeek}</p>
+          </div>
+          <h1 className="flex justify-center text-xl font-bold mb-4">Rover Stats</h1>
+          <div className="flex flex-col items-center justify-center mb-4">
+            <h3 className="text-l font-bold mb-2">Total Favorite Rover Images</h3>
             <p className="text-2xl">{totalRoverImages}</p>
+          </div>
+          <div className="flex flex-col items-center justify-center mb-4">
+            <h3 className="text-l font-bold mb-2">Total Favorite Rover Likes Today</h3>
+            <p className="text-2xl">{roverLikes}</p>
+          </div>
+          <div className="flex flex-col items-center justify-center mb-4">
+            <h3 className="text-l font-bold mb-2">Total Favorite Rover Likes Last Week</h3>
+            <p className="text-2xl">{roverLikesLastWeek}</p>
           </div>
         </div>
       </div>
