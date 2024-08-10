f48d594a2e02ba60b0f65d788feea45c58ef7ab5
Caleb Kuss <kusscall@gmail.com>
2024-08-10 18:24:54 -0500

    fix: index key placement

--- src/components/StaticPlanets/SolarPlanets.tsx
+++ src/components/StaticPlanets/SolarPlanets.tsx
@@ -35,8 +35,8 @@ export default function SolarPlanets() {
 		<div className="flex flex-col justify-center items-center">
 			<h1 className="text-3xl font-bold mb-4">Celestial Bodies</h1>
 			{data.planets.map((planet: Planet, index: number) => (
-				<div className="bg-gray-800 text-white p-4 m-4">
-					<div className="text-xl font-bold mb-4" key={index}>{planet.name}</div>
+				<div className="bg-gray-800 text-white p-4 m-4" key={index}>
+					<div className="text-xl font-bold mb-4">{planet.name}</div>
 					<Image className="mb-4 rounded" src={`/solar-system/${planet.image}`} alt={planet.name} width={400} height={400} />
 					<p>Distance from Sun: {planet.distanceFromSunKm} km</p>
 					<p>Diameter: {planet.diameterKm} km</p>
@@ -48,8 +48,8 @@ export default function SolarPlanets() {
 			}
 
 			{data.dwarfPlanets.map((dwarfPlanet: DwarfPlanet, index: number) => (
-				<div className="bg-gray-800 text-white p-4 m-4">
-					<div className="text-xl font-bold mb-4" key={index}>{dwarfPlanet.name}</div>
+				<div className="bg-gray-800 text-white p-4 m-4" key={index}>
+					<div className="text-xl font-bold mb-4">{dwarfPlanet.name}</div>
 					<Image className="mb-4 rounded" src={`/solar-system/${dwarfPlanet.image}`} alt={dwarfPlanet.name} width={400} height={400} />
 					<p>Distance from Sun: {dwarfPlanet.distanceFromSunKm} km</p>
 					<p>Diameter: {dwarfPlanet.diameterKm} km</p>
@@ -61,8 +61,8 @@ export default function SolarPlanets() {
 			}
 
 			{data.comets.map((comet: Comet, index: number) => (
-				<div className="bg-gray-800 text-white p-4 m-4">
-					<div className="text-xl font-bold mb-4" key={index}>{comet.name}</div>
+				<div className="bg-gray-800 text-white p-4 m-4" key={index}>
+					<div className="text-xl font-bold mb-4">{comet.name}</div>
 					<Image className="mb-4 rounded" src={`/solar-system/${comet.image}`} alt={comet.name} width={400} height={400} />
 					<p>Discovery Year: {comet.discoveryYear}</p>
 					<p>Orbit Period: {comet.orbitPeriodYears} years</p>
