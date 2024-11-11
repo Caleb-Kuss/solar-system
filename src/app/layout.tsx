a8a1c881fa8b6758f57b2e776b09901bac37d406
Caleb Kuss <kusscall@gmail.com>
2024-11-10 21:10:00 -0600

    chore: remove insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,23 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 import AuthProvider from "./context/AuthProvider";
 export const metadata: Metadata = {
   title: "Solar System",
   description:
-    "See a daily Astronomy Picture of the Day (APOD) from NASA. Or explore Mars Rover photos."
+    "See a daily Astronomy Picture of the Day (APOD) from NASA. Or explore Mars Rover photos.",
 };
 
 export default function RootLayout({
-  children
+  children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AuthProvider>
-          {children}
-          <SpeedInsights />
-        </AuthProvider>
+        <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
   );
